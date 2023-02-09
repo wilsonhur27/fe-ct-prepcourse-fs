05b4505c2a3558e726ee4caba75b6e472f243b54
Wilsonhur27 <excalibur2712@gmail.com>
2023-02-09 17:04:30 -0400

    Tareas M03

--- M03 JavaScript Fundamentos/Ejercicio 04.js
+++ M03 JavaScript Fundamentos/Ejercicio 04.js
@@ -25,7 +25,7 @@ function elevar(num, exponent) {
 function redondearNumero(num) {
    // Redondea "num" al entero más próximo y retórnalo.
    // Tu código:
-       return math.round(num);
+       return math.random (num);
 }
 
 function redondearHaciaArriba(num) {

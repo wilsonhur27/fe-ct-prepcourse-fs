f7c3653fc82d3743ccf2fea04d3d12c04c7d1f9a
Wilsonhur27 <excalibur2712@gmail.com>
2023-02-09 16:02:36 -0400

    Tareas M03

--- M03 JavaScript Fundamentos/Ejercicio 05.js
+++ M03 JavaScript Fundamentos/Ejercicio 05.js
@@ -72,16 +72,28 @@ function deEuroAdolar(euro) {
 }
 
 function esVocal(letra) {
+   if (letra.length !== 1) {
+      return "Dato incorrecto";
+    }
+    else if (letra === "a" || letra ==="e" || letra ==="i" ||
+    letra === "o" || letra === "u") {
+      return "Es vocal";
+    }
+    else if(letra !== "a" || letra !=="e" || letra !=="i" ||
+    letra !== "o" || letra !== "u") {
+      return "Dato incorrecto";
+    }
+   }
+
+    
    // Escribe una función que reciba una letra y, si es una vocal, muestre el mensaje “Es vocal”.
    // Si el usuario ingresa un string de más de un caracter debes retornar el mensaje: "Dato incorrecto".
    // Si no es vocal, tambien debe retornar "Dato incorrecto".
    // Tu código:
-   if(letra ==="a" || letra ==="e" || letra ==="i" || letra==="o" || letra==="u") {
-      return "Es vocal"; 
-    } else if (letra.length >1) {
-      return "Dato incorrecto";
-  }
-}
+   
+
+
+
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
